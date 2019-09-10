2207f55cb53563c7b005772109f93ac5cf622783
Grégoire Pineau <lyrixx@lyrixx.info>
2019-09-10 13:58:27 +0200

    Ensure slack is slack when slack call us

--- src/Controller/SlackController.php
+++ src/Controller/SlackController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/** @Route("", defaults={"slack"=true}) */
 class SlackController extends AbstractController
 {
     private $bigBrowser;
---
+++ src/EventSubscriber/SignatureSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class SignatureSubscriber implements EventSubscriberInterface
+{
+    private $signinSecret;
+
+    public function __construct(string $signinSecret)
+    {
+        $this->signinSecret = $signinSecret;
+    }
+
+    public function onRequestEvent(RequestEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        if (!$request->attributes->get('slack', false)) {
+            return;
+        }
+
+        $body = (string) $request->getContent();
+        $signature = $request->headers->get('X-Slack-Signature');
+        $timestamp = $request->headers->get('X-Slack-Request-Timestamp');
+
+        $payload = "v0:$timestamp:$body";
+
+        $signatureTmp = "v0=".hash_hmac('sha256', $payload, $this->signinSecret);
+
+        if ($signatureTmp !== $signature) {
+            $event->setResponse(new Response("You are not slack", 401));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            RequestEvent::class => ['onRequestEvent', 32 - 1],
+        ];
+    }
+}
